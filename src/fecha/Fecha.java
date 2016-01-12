1c76293faf7a8e87a21dabd2909278de10db5613
Admin <Admin@Admin-PC>
2016-01-12 11:16:17 +0100

    primera version para pedir fechas con limites

---
+++ src/fecha/Fecha.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fecha;
+
+/**
+ *
+ * @author Admin
+ */
+public class Fecha {
+    private int dia;
+    private int mes;
+    private int year;
+
+    /**
+     * @return the dia
+     */
+    public int getDia() {
+        return dia;
+    }
+
+    /**
+     * @param dia the dia to set
+     */
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    /**
+     * @return the mes
+     */
+    public int getMes() {
+        return mes;
+    }
+
+    /**
+     * @param mes the mes to set
+     */
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+
+    /**
+     * @return the year
+     */
+    public int getYear() {
+        return year;
+    }
+
+    /**
+     * @param year the year to set
+     */
+    public void setYear(int year) {
+        this.year = year;
+    }
+    
+    public int bisiesto()
+    {
+        int dias=28;
+        
+        if ((year%4==0 && year %100!=0)|| (year%400==0) )
+            dias =29;
+            
+            return dias;
+    }
+    
+        public int bisiesto(int a)
+    {
+        int dias=28;
+        
+        if ((a%4==0 && a %100!=0)|| (a%400==0) )
+            dias =29;
+            
+            return dias;
+    }
+        public boolean comprobarFecha(int d, int m, int y){
+            
+            boolean resultado=true;
+            int[]diaMes={31,28,31,30,31,30,31,31,30,31,30,31};
+            if (y<0){
+                resultado=false;
+                }
+                
+            else{
+                diaMes[1]=bisiesto(y);
+                if (m<1 ||m>12)
+                    {
+                    resultado=false;
+                    }
+                    else
+                    {
+                     if (d<1 ||d> diaMes [m-1])
+                        {
+                        resultado=false; 
+                        }
+                    }
+                }
+            return resultado;
+        }
+}
